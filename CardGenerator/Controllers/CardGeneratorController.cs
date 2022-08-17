9ca65eedacd4a953c8f8830d1bfe51b9165c419f
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-08-17 18:31:24 -0300

    first step project

---
+++ CardGenerator/Controllers/CardGeneratorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CardGenerator.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CardGeneratorController : ControllerBase
+{
+    [HttpGet(Name = "GetAnything")]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
---
+++ CardGeneratorRepository/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CardGeneratorRepository;
+
+public class Class1
+{
+}
